bb11ec609ce8db92ad37f4ba9c7c5f4bb9c69299
andro2ss <a.brzeski@hotmail.com>
2021-12-15 14:02:21 +0100

    prepared class Furry and Coin

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 import "../sass/main.scss";
 
 console.log("Test");
+
+class Furry {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.direction = "right";
+    }
+
+}
+
+class Coin {
+    constructor() {
+        this.x = Math.floor(Math.random() * 10);
+        this.y = Math.floor(Math.random() * 10);
+    }
+}
